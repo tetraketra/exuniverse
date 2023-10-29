13826cbe4ad376d9e38555a1f061089c7e81bd89
Tetra Silverberg <tetra.silv@gmail.com>
2023-10-29 02:30:20 -0400

    Caps are gross.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from . import db
 from . import os_addons
 
 
-## APP ## TODO: config files, testing, actually secret secrets
+## app TODO: config files, testing, actually secret secrets
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='CED0567CB8B29657FE1EB8132FAE5707FCD4C67DBF744CDDED5BDE7C0BD49594',
@@ -19,13 +19,13 @@ os_addons.ensure_dir(app.instance_path)
 
 db.init_app(app)
 
-## API ## TODO: all of this lmao
+## api TODO: all of this lmao
 api = Api(app)
 
-## LOGINMANAGER ## TODO: all of this lmao
+## login manager TODO: all of this lmao
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-## RUN ##
+## run
 if __name__ == "main":
     app.run(debug = True) # FIXME: Never deploy with `debug = True`!
